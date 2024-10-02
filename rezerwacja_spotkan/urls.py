5b0a56dbb495f6ec04229c3b2552fa0e664ffca1
Shanoxx <lukasgrot98@gmail.com>
2024-10-02 19:12:34 +0200

    Tworzenie stron internetowych - strona glowna aplikacji

--- rezerwacja_spotkan/urls.py
+++ rezerwacja_spotkan/urls.py
@@ -5,4 +5,9 @@ app_name = 'rezerwacja_spotkan'
 urlpatterns = [
     #Strona główna.
     path('', views.index, name='index'),
+    path('topics/', views.topics, name='topics'),
+    #Strona szczegolowa dotyczaca pojedynczego tematu.
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
+    #Strona przeznaczona do dodawania nowego tematu.
+    path('new_topic/', views.new_topic, name='new_topic'),
 ]
--- rezerwacja_spotkan/views.py
+++ rezerwacja_spotkan/views.py
@@ -1,5 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Topic
+from .forms import TopicForm
 
 def index(request):
     """Strona główna dla aplikacji rezerwacja_spotkan"""
     return render(request, 'rezerwacja_spotkan/index.html')
+
+def topics(request,):
+    """Wyswietlanie wszytkich tematow"""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'rezerwacja_spotkan/topics.html', context)
+
+def topic(request, topic_id):
+    """Wyswietla pojedynczy temat i wszystkie powiazane z nim wpisy"""
+    topic = Topic.objects.get(id=topic_id)
+    opisy = topic.opis_set.order_by('-date_added')
+    context = {'topic': topic, 'opisy': opisy}
+    return render(request, 'rezerwacja_spotkan/topic.html', context)
+
+def new_topic(request):
+    """Dodaj nowy temat"""
+    if request.method != 'POST':
+        #Nie przekazano zadnych danych nalezy utworzyc pusty formularz
+        form = TopicForm()
+    else:
+        #Przekazano dane za pomoca zadania POST nalezy je przetworzyc
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('rezerwacja_spotkan:topics')
+#Wyswietlanie pustego formularza
+    context = {'form': form}
+    return render(request, 'rezerwacja_spotkan/new_topic.html', context)
