35d4fe1b400aee578ecd23dbeff1e140dd096bbc
Shanoxx <lukasgrot98@gmail.com>
2024-10-22 21:52:20 +0200

    aktualizacja staticfiles

--- rs_project/settings.py
+++ rs_project/settings.py
@@ -128,10 +128,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    BASE_DIR / "rezerwacja_spotkan/static",
+    os.path.join(BASE_DIR, "rezerwacja_spotkan/static"),
 ]
 
 # Default primary key field type
